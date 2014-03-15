af0d691c6761c1dfd64ee691757dac42e0ba77bc
Jeffrey Louden <jeff@jefflouden.com>
2014-03-15 16:08:45 -0700

    all the changes

--- js/core.js
+++ js/core.js
@@ -2,19 +2,28 @@
     "use strict";
 
     var
+        intro = doc.getElementById('intro'),
+        overlay = doc.getElementById('overlay'),
         resume = doc.getElementById('resume'),
         showResume = doc.getElementById('show-resume'),
         closeResume = doc.getElementById('close-resume');
 
     function close() {
         if ($(resume).hasClass('visible')) {
+            $(overlay).removeClass('visible');
             $(resume).addClass('fadeOutUp').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
                 $(this).removeClass('visible fadeOutUp');
+                $(intro).removeClass('fadeOut').addClass('fadeIn');
             });
         }
     }
 
     $(closeResume).on('click', function(e) {
+        e.preventDefault();
+        close();
+    });
+
+    $(overlay).on('click', function(e) {
         e.preventDefault();
         close();
     });
@@ -27,8 +36,10 @@
 
     $(showResume).on('click', function(e) {
         e.preventDefault();
+        $(overlay).addClass('visible');
+        $(intro).removeClass('fadeInLeft').addClass('fadeOut');
         $(resume).addClass('visible fadeInDown').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
-            $(this).removeClass('fadeInDown');
+            $(this).removeClass('fadeInDown fadeIn');
         });
     });
 
